96eca8e967661d89e3f9ded35354042731bf4bab
chairul <chairul@students.mikroskil.ac.id>
2025-02-22 14:57:54 +0700

    add navbar

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import NavBar from "./components/Navbar";
+// import imagePath from "./assets/your_image_icon.png";
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+export default App;
